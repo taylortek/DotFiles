081e0c0ac6b8abc3aff7949eb9652502a240572f
Taylor Kerrihard <taylor.kerrihard@gmail.com>
2022-10-05 01:51:24 -0400

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,16 @@
+set fish_greeting
+function fish_mode_prompt
+end
+
+set -g theme_display_rw no
+set -g theme_display_group no
+set -g theme_display_time yes
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+
+set -U fish_key_bindings fish_default_key_bindings
+
+nvm use stable > /dev/null
